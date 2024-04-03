f65ee001e2548064f7327f3871faaa6a3075c6d0
Hammadabbasi17 <165670673+Hammadabbasi17@users.noreply.github.com>
2024-04-03 08:45:57 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,45 @@
+// import inquirer from "inquirer";
+
+
+// let input = await inquirer.prompt([
+//     {
+//         type: 'list',
+//         name: 'calculator',
+//         message: 'choose the  Arithmetic operation',
+//         choices: ["Addition", "Substraction", "multiplication", "Division"],
+
+//     },
+
+//     {
+//         type: 'number',
+//         name: 'num1',
+//         message: 'Enter your first number',
+//     }
+//     ,
+//     {
+//         type: 'number',
+//         name: 'num2',
+//         message: 'Enter your Second number',
+//     },
+// ])
+// if (input.calculator === "Addition") {
+//     console.log(`your number is ${input.num1 + input.num2}`);
+
+// }
+
+// else if (input.calculator === "Substraction") {
+//     console.log(`your number is ${input.num1 - input.num2}`);
+
+// }
+// else if (input.calculator === "multiplication") {
+//     console.log(`your number is ${input.num1 * input.num2}`);
+
+// }
+// else if (input.calculator === "Division") {
+//     console.log(`your number is ${input.num1 / input.num2}`);
+
+// }
+
+
+
+
